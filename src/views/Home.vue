94a644c5a6b297503f72ed9fec65284b9920d7bf
feifei <feiyinn29@gmail.com>
2022-06-09 16:00:51 +0700

    edit home+about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
-  <v-container fluid class="">
-    <v-row class="d-flex justify-center tw-py-44" align="center">
-      <div
-        class="tw-font-bold tw-text-transparent tw-text-4xl tw-uppercase tw-bg-clip-text tw-bg-gradient-to-r tw-from-purple-400 tw-to-pink-600">
-        welcome home</div>
+  <v-container>
+    <v-row class="d-flex justify-center tw-py-40" no-gutters>
+      <p class="tw-font-bold tw-text-transparent tw-text-4xl tw-uppercase tw-bg-clip-text tw-bg-gradient-to-r tw-from-purple-400 tw-to-pink-600"> welcome home </p>
     </v-row>
   </v-container>
 </template>
