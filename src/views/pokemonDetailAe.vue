7e147b3963d6cc963eb414d3cedfc2a5b188bb74
Pasit Tiwa <pasit.tiwa40@gmail.com>
2022-06-15 14:24:09 +0700

    Ae

---
+++ src/views/pokemonDetailAe.vue
@@ -0,0 +1,20 @@
+<template>
+  <Suspense>
+    <pokemon-with-id-ae :id="id"></pokemon-with-id-ae>
+    <template #fallback> Loading... </template>
+  </Suspense>
+</template>
+
+<script>
+import PokemonWithIdAe from "@/components/PokemonWithIdAe.vue";
+export default {
+  props: ["id"],
+
+  components: {
+    PokemonWithIdAe,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
